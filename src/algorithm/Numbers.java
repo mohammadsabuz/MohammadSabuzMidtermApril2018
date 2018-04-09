ad77e552caa1cf14fcfc08144d0216c1fae57a2d
mohammadsabuz <mohammadsabuz@gmail.com>
2018-04-09 14:53:41 -0400

    commiting again

--- src/algorithm/Numbers.java
+++ src/algorithm/Numbers.java
@@ -21,55 +21,119 @@ public class Numbers {
 	 */
 
 	public static void main(String[] args) throws Exception {
-		
-		int [] num = new int[10];
+
+		int [] num = new int[1000000];
+
 		storeRandomNumbers(num);
 		ConnectDB connectDB = new ConnectDB();
 
 		//Selection Sort
 		Sort algo = new Sort();
 		algo.selectionSort(num);
+
+		//selection Sort
 		long selectionSortExecutionTime = algo.executionTime;
 		System.out.println("Total Execution Time of "+ num.length + " numbers in Selection Sort take: " + selectionSortExecutionTime + " milli sec");
-        connectDB.insertDataFromArrayToMySql(num, "selection_sort", "SortingNumbers");
-        List<String> numbers = connectDB.readDataBase("selection_sort", "SortingNumbers");
-        printValue(numbers);
+		connectDB.insertDataFromArrayToMySql(num, "selection_sort", "SortingNumbers");
+		List<String> numbers = connectDB.readDataBase("selection_sort", "SortingNumbers");
+		printValue(numbers);
 		int n = num.length;
 		randomize (num, n);
-
-		//Come to conclusion about which Sorting Algo is better in given data set.
 
 		//Insertion Sort
 		algo.insertionSort(num);
 		long insertionSortExecutionTime = algo.executionTime;
-		System.out.println("Total Execution Time " + num.length + " numbers in Insertion Sort takes: " + insertionSortExecutionTime + " milli sec");
+		System.out.println("Total Execution Time of " + num.length + " numbers in Insertion Sort take: " + insertionSortExecutionTime + " milli sec");
+		connectDB.insertDataFromArrayToMySql(num, "Insertion_sort", "SortingNumbers");
 
-		// Bubble Sort
-		algo.insertionSort(num);
+		printValue(numbers);
+		randomize (num, n);
+
+		//bubble Sort
+		algo.bubbleSort(num);
 		long bubbleSortExecutionTime = algo.executionTime;
-		System.out.println("Total Execution Time " + num.length + " numbers in Bubble Sort takes: " + bubbleSortExecutionTime + " milli sec");
+		System.out.println("Total Execution Time of " + num.length + " numbers in Bubble Sort take: " + bubbleSortExecutionTime + " milli sec");
+		connectDB.insertDataFromArrayToMySql(num, "Bubble_sort", "SortingNumbers");
 
-		// Quick Sort
-//		algo.quickSort(num, findLow[num], findHigh[num]);
-//		long quickSortExecutionTime = algo.executionTime;
-//		System.out.println("Total Execution Time " + num.length + " numbers in Quick Sort takes: " + quickSortExecutionTime + " milli sec");
+		printValue(numbers);
+		randomize (num, n);
 
-		// Heap Sort
+		//Heap Sort
 		algo.heapSort(num);
 		long heapSortExecutionTime = algo.executionTime;
-		System.out.println("Total Execution Time " + num.length + " numbers in Heap Sort takes: " + heapSortExecutionTime + " milli sec");
+		System.out.println("Total Execution Time of " + num.length + " numbers in Heap Sort take: " + heapSortExecutionTime + " milli sec");
+		connectDB.insertDataFromArrayToMySql(num, "Heap_sort", "SortingNumbers");
 
-		// Bucket Sort
+		printValue(numbers);
+		randomize (num, n);
+
+		//Bucket sort
 		algo.bucketSort(num);
 		long bucketSortExecutionTime = algo.executionTime;
-		System.out.println("Total Execution Time " + num.length + " numbers in Bucket Sort takes: " + bucketSortExecutionTime + " milli sec");
+		System.out.println("Total Execution Time of " + num.length + " numbers in bucket Sort take: " + bucketSortExecutionTime + " milli sec");
+		connectDB.insertDataFromArrayToMySql(num, "bucket_sort", "SortingNumbers");
+
+		printValue(numbers);
+		randomize (num, n);
+
+		//quick sort
+		algo.quickSort(num, findLow(num), findHigh(num));
+		long quickSortExecutionTime = algo.executionTime;
+		System.out.println("Total Execution Time of " + num.length + " numbers in Quick Sort take: " + quickSortExecutionTime + " milli sec");
+		connectDB.insertDataFromArrayToMySql(num, "quick_sort", "SortingNumbers");
+
+		printValue(numbers);
+		randomize (num, n);
+
+		//merge Sort
+		algo.MergeSort(num);
+		long mergeSortExecutionTime = algo.executionTime;
+		System.out.println("Total Execution Time of " + num.length + " numbers in merge Sort take: " + mergeSortExecutionTime + " milli sec");
+		connectDB.insertDataFromArrayToMySql(num, "merge_sort", "SortingNumbers");
+
+		printValue(numbers);
+		randomize (num, n);
+
+		//Shell Short
+		algo.shellSort(num);
+		long shellSortExecutionTime = algo.executionTime;
+		System.out.println("Total Execution Time of " + num.length + " numbers in Shell Sort take: " + shellSortExecutionTime + " milli sec");
+		connectDB.insertDataFromArrayToMySql(num, "Shell_sort", "SortingNumbers");
+
+		printValue(numbers);
+		randomize (num, n);
+
+		//Come to conclusion about which Sorting Algo is better in given data set.
+
 	}
+
 	public static void storeRandomNumbers(int [] num){
 		Random rand = new Random();
 		for(int i=0; i<num.length; i++){
-			num[i] = rand.nextInt(10);
+			num[i] = rand.nextInt(1000000);
 		}
 	}
+
+	public static int findLow(int[] array){
+		int low = array[0];
+		for(int n: array){
+			if(array[n] < low){
+				low = array[n];
+			}
+		}
+		return low;
+	}
+
+	public static int findHigh(int[] array){
+		int high = array[0];
+		for(int n: array){
+			if(array[n] > high){
+				high = array[n];
+			}
+		}
+		return high;
+	}
+
 	public static void randomize( int arr[], int n)
 	{
 		Random r = new Random();
@@ -82,25 +146,7 @@ public class Numbers {
 			arr[j] = temp;
 		}
 	}
-	public static int findLow(int[]array){
-		int low = array[0];
-		for (int n:array){
-			if (array[n] < low){
-				low = array[n];
-			}
-		}
-		return low;
-	}
 
-	public static int findHigh(int[]array){
-		int high = array[0];
-		for (int n:array){
-			if (array[n] > high){
-				high = array[n];
-			}
-		}
-		return high;
-	}
 	public static void printValue(List<String> array){
 		for(String st:array){
 			System.out.println(st);
--- src/algorithm/UnitTestSorting.java
+++ src/algorithm/UnitTestSorting.java
@@ -3,7 +3,6 @@ package algorithm;
 import org.testng.Assert;
 
 public class UnitTestSorting {
-
     /*
       This class is about Unit testing for Sorting Algorithm.
      */
@@ -13,16 +12,79 @@ public class UnitTestSorting {
         //Create Sort object
         Sort sort = new Sort();
         //apply unsorted array to selectionSort.
+
+        // selection sort
         sort.selectionSort(unSortedArray);
         //verify if the unsorted array is sorted by the selection sort algorithm.
         try {
-            Assert.assertEquals(sortedArray, unSortedArray, "Array is not Sorted");
+            Assert.assertEquals(sortedArray, unSortedArray, "Selection sort array is not sorted!");
         }catch(Exception ex){
             ex.getMessage();
         }
 
         //Now do for rest of the algorithm...................below
 
+        //insertion sort
+        sort.insertionSort(unSortedArray);
+        try{
+            Assert.assertEquals(sortedArray, unSortedArray, "Insertion sort array is not sorted!");
+        }catch (Exception ex){
+            ex.getMessage();
+        }
 
+        sort.bubbleSort(unSortedArray);
+        try {
+            Assert.assertEquals(sortedArray, unSortedArray, "Bubble sort array is not sorted!");
+        }catch(Exception ex){
+            ex.getMessage();
+        }
+
+        // merge sort
+        sort.MergeSort(unSortedArray);
+        try{
+            Assert.assertEquals(sortedArray, unSortedArray, "Merge sort array is not sorted!");
+        }catch (Exception ex){
+            ex.getMessage();
+        }
+
+//        // quick sort
+//        sort.quickSort(unSortedArray);
+//        try{
+//            Assert.assertEquals(sortedArray, unSortedArray, "Quick sort array is not sorted!");
+//        }catch (Exception ex){
+//            ex.getMessage();
+//        }
+
+        // heap sort
+        sort.heapSort(unSortedArray);
+        try{
+            Assert.assertEquals(sortedArray, unSortedArray, "Heap sort array is not sorted!");
+        }catch (Exception ex){
+            ex.getMessage();
+        }
+
+        // bucket sort
+        sort.bucketSort(unSortedArray);
+        try{
+            Assert.assertEquals(sortedArray, unSortedArray, "Bucket sort array is not sorted!");
+        }catch (Exception ex){
+            ex.getMessage();
+        }
+
+        // get max
+        sort.getMax(unSortedArray);
+        try{
+            Assert.assertEquals(sortedArray, unSortedArray, "Quick sort array is not sorted!");
+        }catch (Exception ex){
+            ex.getMessage();
+        }
+
+        // shell sort
+        sort.shellSort(unSortedArray);
+        try{
+            Assert.assertEquals(sortedArray, unSortedArray, "Quick sort array is not sorted!");
+        }catch (Exception ex){
+            ex.getMessage();
+        }
     }
 }
